699d716476e07003cf4cb554b07c0955188628d7
soroush62 <112301194+soroush62@users.noreply.github.com>
2024-10-04 10:21:23 +0300

    ctrate Incidents Over Time vs Liquid Recovery chart

--- Python Files/MonthlyIncidentsPointLine.py
+++ Python Files/MonthlyIncidentsPointLine.py
@@ -1,26 +1,33 @@
 # import lightningchart as lc
 # import pandas as pd
 # import numpy as np
+# from datetime import datetime
 
 # lc.set_license(open('../license-key').read())
 
 # data = pd.read_csv('Dataset/database.csv')
 # data = pd.DataFrame(data)
-# # print(data)
 
-# accident = data['Accident Year'].unique()
-# # print(accident)
+# data['Accident Date/Time'] = pd.to_datetime(data['Accident Date/Time'])
+# accident_date_year = data['Accident Date/Time'].dt.year
+# accident_date_year_month = data['Accident Date/Time'].dt.to_period('M')
+# accident_date_month = data['Accident Date/Time'].dt.month
 
+# print(data['Accident Date/Time'])
+# print(f' Accident Date/Time in year: {accident_date_year}')
+# print(f' Accident Date/Time in year_month: {accident_date_year_month}')
+# print(f' Accident Date/Time in month: {accident_date_month}')
 
 
 # # let's count the number of accidents in each year
-# accidents_in_each_year = data['Accident Year'].value_counts().tolist()
-
+# accidents_in_each_year = data['Accident Year'].value_counts().sort_index()
+# accident_date_year_ms = [int(datetime(year, 1, 1).timestamp()) * 1000 for year in accidents_in_each_year.index]
+# print(f'Accidents in each year in milliseconds: {accident_date_year_ms}')
 
 # chart=lc.ChartXY(theme=lc.Themes.Dark, title="Accidents in each year")
 # series = chart.add_point_line_series()
-# series.add(accident, accidents_in_each_year)
-# x_axis=chart.get_default_x_axis().set_tick_strategy('Numeric')
+# series.add(accident_date_year_ms, accidents_in_each_year.tolist())
+# x_axis=chart.get_default_x_axis().set_tick_strategy('DateTime', utc=True)
 # chart.open()
 
 
@@ -40,7 +47,7 @@ file_path = 'Dataset/database.csv'  # Update this with your actual file path
 data = pd.read_csv(file_path)
 
 # Convert the "Accident Date/Time" column to datetime format
-data['Accident Date/Time'] = pd.to_datetime(data['Accident Date/Time'], format='%m/%d/%Y %H:%M')
+data['Accident Date/Time'] = pd.to_datetime(data['Accident Date/Time'])
 
 # Extract month and year for grouping
 data['year_month'] = data['Accident Date/Time'].dt.to_period('M')
@@ -75,5 +82,6 @@ point_line_series.set_point_size(8).set_point_shape('Triangle').set_point_color(
 # Add data to the series
 point_line_series.add(x_values, y_values)
 
-# Open the chart
 chart.open()
+
+
